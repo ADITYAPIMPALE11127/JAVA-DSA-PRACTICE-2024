4a9e5edf5bd38fb8ad2aa5f98afa91e194a4b205
ADITYAPIMPALE11127 <pimpaleaditya2@gmail.com>
2024-07-28 15:31:43 +0530

    prime number commit # 3

---
+++ primeNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class primeNumber {
+
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = input.nextInt();
+        // Check if the number is 1
+        if (num == 1) {
+            System.out.print("Not a prime number");
+            return;  // Exit the program early
+        }
+
+        int temp = 0;
+        for (int i = 2; i <= num - 1; i++) {
+            if (num % i == 0) {
+                temp = temp + 1;
+            }
+        }
+
+        if (temp > 0) {
+            System.out.print("Not a prime number");
+        } else {
+            System.out.print(num + " is a Prime Number");
+        }
+    }
+}
